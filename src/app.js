3bee5548f5346f168edf204b1df68952aba2f609
Ary1993 <43453934+Ary1993@users.noreply.github.com>
2024-01-02 15:10:58 +0000

    Correciones

--- src/app.js
+++ src/app.js
@@ -7,12 +7,16 @@ let generateDomain = () => {
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let finalArray = [];
 
   for (let i = 0; i < pronoun.length; i++) {
     for (let j = 0; j < adj.length; j++) {
       for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+        finalArray.push(pronoun[i] + adj[j] + noun[k] + ".com");
       }
     }
   }
+  return (
+    "<ul>" + finalArray.map(domain => `<li>${domain}</li>`).join("") + "</ul>"
+  );
 };
